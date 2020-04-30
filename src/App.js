c63aea3c3285a95554ef5acf5bf2e5894fa0b0ff
unknown <miloszz87@gmail.com>
2020-04-29 21:23:27 -0400

    homework

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-      <PhotosInfo />
-    </div>
+      <div className="App">
+        <div class="nav-container">
+          <h1>NASA <span role="img" aria-label='go!'>🚀</span></h1>
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">Search</a>
+          </nav>
+        </div>
+        <PhotosInfo />
+        <div class="footer-container">
+          <footer>
+            <p>© 2020<br/>Milosz Samec</p>
+            <div class="social-icons">
+              <a href="#">GitHub</a>
+              <a href="#">Linkedin</a>
+              <a href="#">Facebook</a>
+            </div>
+          </footer>
+        </div>
+      </div>
   );
 } 
  
